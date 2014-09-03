99780615dd9f9c42d8a26ca65487622802dfb99d
IanMcT <ian.mctavish@tldsb.on.ca>
2014-09-02 21:32:23 -0400

    Program to create a console application
    First version.

---
+++ ics3u_unit1_day1_helloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Program: Hello World
+ * Author:  
+ * Date: Sept 2, 2014
+ * Simple program to demo how to create a console program.
+ */
+namespace ics3u_unit1_day1_helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //get the name from the user
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+
+            //ReadLine keeps the program running until
+            //the user enters something - otherwise
+            //it closes immediately
+            Console.ReadLine();
+        }
+    }
+}
